e936ddadb74a8af92eabe65ceeed2bbed085d943
NerminImamovic <nimamovic9@gmail.com>
2022-05-11 22:14:02 +0200

    message sender

---
+++ handler.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const AWS = require("aws-sdk");
+
+module.exports.sendMessage = (event, context) => {
+  const sendSMSPromise = new AWS.SNS()
+    .publish({
+      Message: 'Hi there!',
+      PhoneNumber: '+38761xxxxx',
+      MessageAttributes: {
+        'AWS.SNS.SMS.SenderID': {
+          DataType: 'String',
+          StringValue: 'Sender'
+        },
+      },
+  }).promise();
+
+  sendSMSPromise
+    .then((data) => {
+      console.log("SMS successfully sent. ", JSON.stringify(data));    
+    })
+    .catch((err) => {
+      console.error(err, err.stack);
+    });
+};
